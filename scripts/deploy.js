53cf836168d39a462add12dc04184efa7d34b72a
Anthony <antharithm707@gmail.com>
2023-01-15 07:14:33 -0500

    updated uints

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,12 @@
 // We require the Hardhat Runtime Environment explicitly here. This is optional
 // but useful for running the script in a standalone fashion through `node <script>`.
-//
+
+// npx hardhat run --network localhost scripts/deploy.js
+
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
+
 const hre = require("hardhat");
 
 async function main() {
@@ -13,11 +16,8 @@ async function main() {
 
   await ctm.deployed();
 
-  console.log(
-    `CTM was deployed to: ${ctm.address}`
-  );
+  console.log(`CTM was deployed to: ${ctm.address}`);
 }
-
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
