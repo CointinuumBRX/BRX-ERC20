0f0fc01aceee33828facac3ff305517778a868b2
Anthony <antharithm707@gmail.com>
2023-01-17 08:55:41 -0500

    updated hh config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,21 @@
-require("@openzeppelin/hardhat-upgrades");
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
-  gasReporter: {
-      enabled: false,
-  }
+  networks: {
+    goerli: {
+
+      url: process.env.NODE_ENDPOINT,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY
+  },
+  gasReporter: {
+    enabled: false,
+  },
 };
